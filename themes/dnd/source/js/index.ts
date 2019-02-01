d14fb43405046b687aa5d8d46fb01befdaba8971
Matic Leva <matic.leva@inova.si>
2019-02-01 17:00:39 +0100

    Improve image overlay

--- themes/dnd/source/js/index.ts
+++ themes/dnd/source/js/index.ts
@@ -1,7 +1,43 @@
-document.addEventListener('click', (event) => {
-    const classList = (event.target as HTMLElement).classList;
-    if (classList.contains('img-center') &&
-        classList.contains('resizable')) {
-        classList.toggle('fullscreen');
+class Awakening {
+
+    private readonly overlay: HTMLElement;
+
+    constructor() {
+        this.overlay = document.createElement('div');
+        this.overlay.classList.add('image-preview');
+        this.overlay.setAttribute('image-preview', '');
+        document.body.append(this.overlay);
     }
+
+    public toggleImage(src?: string, alt?: string) {
+        if (this.overlay.classList.contains('opened')) {
+            this.closeImage();
+        } else {
+            this.overlay.innerHTML = `<img src="${src}" alt="${alt}" image-preview>`;
+            this.overlay.classList.add('opened');
+        }
+    }
+
+    public closeImage() {
+        this.overlay.classList.remove('opened');
+    }
+
+}
+
+
+declare const awakening: Awakening;
+(window as any).awakening = new Awakening();
+
+document.addEventListener('click', (event) => {
+    const target = (event.target as HTMLElement);
+
+    const hasClass = (classname: string, t = target) => t.classList.contains(classname);
+    const hasAttribute = (attribute: string, t = target) => t.hasAttribute(attribute);
+
+    if (hasClass('img-center') && hasClass('resizable')) {
+        const image = (event.target as HTMLImageElement);
+        awakening.toggleImage(image.src, image.alt);
+    }
+    if ((hasAttribute('image-preview')))
+        awakening.toggleImage();
 });
