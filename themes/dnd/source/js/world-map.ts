a50c47d0132f7e89b401a79abd8637f4809f85f9
Matic Leva <matic.leva@inova.si>
2019-07-30 09:27:52 +0200

    Integrate Tookania map as leaflet

--- themes/dnd/source/js/world-map.ts
+++ themes/dnd/source/js/world-map.ts
@@ -1,18 +1,35 @@
-interface JQuery {
-    worldMap(): WorldMap;
-}
+import {CRS, imageOverlay, LatLngBoundsLiteral, map, Map} from 'leaflet';
 
 class WorldMap {
 
-    private readonly element: JQuery;
+    private readonly popup: JQuery;
+    private readonly map: Map;
 
     constructor() {
-        this.element = $('<div class="world-map"><div class="leaflet"></div></div>');
-        $(document.body).append(this.element);
+        this.popup = $('<div class="world-map"><div class="leaflet"></div></div>');
+        $(document.body).append(this.popup);
+
+        const bounds: LatLngBoundsLiteral = [[0, 0], [3508, 4961]];
+        this.map = map(this.popup.find('.leaflet').get(0), {
+            crs: CRS.Simple,
+            minZoom: -2,
+            maxZoom: 2,
+            zoomControl: false,
+            maxBoundsViscosity: 1.0,
+        });
+        imageOverlay('Tookania.png', bounds).addTo(this.map);
+        this.map.setMaxBounds(bounds);
+        this.map.fitBounds(bounds);
+
+        $(document).on('keyup', (e) => {
+            if (e.which === 27 && this.popup.hasClass('opened'))
+                this.toggle();
+        });
+
     }
 
     public toggle() {
-        this.element.toggleClass('opened');
+        this.popup.toggleClass('opened');
         $(document.body).toggleClass('no-scroll');
     }
 }
